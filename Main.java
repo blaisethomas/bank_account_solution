199d2c1e98d999e039443935d8894cc650e1233e
Blaise Thomas <blaisegthomas@gmail.com>
2016-04-19 23:12:23 -0700

    setup

---
+++ Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by blaise on 4/19/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // create a new class for a bank account
+        // create fields for the account number, balance, customer, name, email and phone number
+        // create all getters and setters
+        // create two additional methods
+        // 1. to allow the customer to deposit funds(increment the balance)
+        // 2. to allow the customer to withdraw funds, but not become overdrawn (balance < 0)
+    }
+}
